6b8f0cb8433c2ad5a9afccba186bd5b501343fe8
dimas.irgiansyah <dimas.irgiansyah@eluon.com>
2021-01-10 07:47:52 +0700

    profile download when chat incoming

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const venom = require('venom-bot');
 const path = require('path');
 const fs = require('fs');
 const mime = require('mime-types');
+const request = require('request');
 
 // for date human
 const timestampToDate = (tm) => {
@@ -10,17 +11,28 @@ const timestampToDate = (tm) => {
     return after = before.toLocaleString();
 }
 
+// get time now
 const getDateNow = () => {
     const date_ob = new Date();
     const date = ("0" + date_ob.getDate()).slice(-2);
     const month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
     const year = date_ob.getFullYear();
-    return year + month + date;
+    const hour = date_ob.getHours();
+    const minute =  date_ob.getMinutes();
+    const second = date_ob.getSeconds();
+    return year + month + date + hour + minute + second;
 }
 
 // for regex only number
 const onlyNumber = (text) => {
     return text.replace(/\D/g, '');
+}
+
+// download image from uri
+const downloadImageFromUri = (uri, fileName, callback) => {
+    request.head(uri, (err, res, body) => {
+        request(uri).pipe(fs.createWriteStream(fileName)).on('close', callback);
+    });
 }
 
 // convert array to json
@@ -66,6 +78,7 @@ const startClientVenom = async (client) => {
 
     // message incoming
     client.onMessage(async (msg) => {
+
         // if message not from group chat and dont broadcast
         if (msg.isGroupMsg === false && msg.from != "status@broadcast") {
 
@@ -80,26 +93,56 @@ const startClientVenom = async (client) => {
             let imgInChat = "-";
             let senderPict = msg.sender.profilePicThumbObj.imgFull;
             let mediaStatus = msg.isMedia;
+            let photoProfile = client.getProfilePicFromServer(msg.chatId);
+            let pathPhotoProfile = "-";
 
             __dirname;
 
-            // if chat with media
-            // create folder for media
+            // create folder json
+            const saveFolderJson = path.relative(__dirname, "json");
+            if (!fs.existsSync(saveFolderJson)) {
+                fs.mkdirSync(saveFolderJson);
+            }
+            // create folder images
             const folderImage = path.relative(__dirname, "images");
             if (!fs.existsSync(folderImage)) {
                 fs.mkdirSync(folderImage);
             }
+            // create sub folder images -> media
             const saveFolderProfileImages = path.relative(__dirname, "images/media");
             if (!fs.existsSync(saveFolderProfileImages)) {
                 fs.mkdirSync(saveFolderProfileImages);
             }
+            // create sub folder images -> profile
+            const saveFolderProfilePhoto = path.relative(__dirname, "images/profile");
+            if (!fs.existsSync(saveFolderProfilePhoto)) {
+                fs.mkdirSync(saveFolderProfilePhoto);
+            }
+            // create file json
+            if (!fs.existsSync("json/chattome.json")) {
+                fs.closeSync(fs.openSync("json/chattome.json", "w"));
+            }
+
+            // save photo profile
+            if (senderPict == undefined || senderPict == null || senderPict == "") {
+                pathPhotoProfile = "Kosong"
+            } else {
+                pathPhotoProfile = `${saveFolderProfilePhoto}${path.sep}${getDateNow()}.${sender}.profile.jpg`;
+                photoProfile.then((result) => {
+                    downloadImageFromUri(result, pathPhotoProfile, () => {
+                        console.log(`Profile ${senderName} downloading done...`);
+                    });
+                });
+            }
+
+            // if chat with media
             // save media
             if (msg.isMedia === true || msg.isMMS === true) {
                 body = msg.caption;
                 const buffer = await client.decryptFile(msg);
                 // At this point you can do whatever you want with the buffer
                 // Most likely you want to write it into a file
-                const fileName = `${saveFolderProfileImages}${path.sep}${msg.t}.${sender}.media.${mime.extension(msg.mimetype)}`;
+                const fileName = `${saveFolderProfileImages}${path.sep}${getDateNow()}.${sender}.media.${mime.extension(msg.mimetype)}`;
                 imgInChat = fileName;
                 await fs.writeFileSync(fileName, buffer, (err) => {
                     console.log(err)
@@ -109,23 +152,13 @@ const startClientVenom = async (client) => {
             // show basic information to console
             console.log("---------------------------------");
             console.log("Waktu: ", time);
+            console.log("Photo: ", pathPhotoProfile);
             console.log("Dari: ", senderName);
             console.log("Pesan: ", body);
             if (mediaStatus === true) {
                 console.log("Media : ", imgInChat);
             }
             console.log("---------------------------------");
-
-            // check exists folder json
-            const saveFolderJson = path.relative(__dirname, "json");
-            if (!fs.existsSync(saveFolderJson)) {
-                fs.mkdirSync(saveFolderJson);
-            }
-
-            // check exists file json
-            if (!fs.existsSync("json/chattome.json")) {
-                fs.closeSync(fs.openSync("json/chattome.json", "w"));
-            }
 
             // read data from file json
             fs.readFile(`${saveFolderJson}${path.sep}chattome.json`, 'utf8', function readFileCallback(err, data) {
@@ -145,7 +178,7 @@ const startClientVenom = async (client) => {
                         receiverName: receiverName,
                         body: body,
                         imgInChat: imgInChat,
-                        senderPict: senderPict
+                        senderPict: pathPhotoProfile
                     })
                     // create file json
                     fs.writeFile(`${saveFolderJson}${path.sep}chattome.json`, JSON.stringify(logMessage), (werr) => {
